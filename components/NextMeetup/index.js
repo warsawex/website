34dac50dd6d0a5c38c84d8e7dd4372d2ee1523fc
anitakniaziew <anita.kniaziew@gmail.com>
2019-06-20 15:59:15 +0200

    add next meetup agenda

--- components/NextMeetup/index.js
+++ components/NextMeetup/index.js
@@ -10,10 +10,10 @@ const Meetup = () => (
       <div className={style.details}>
         <div className={style.num}>
           <div>#</div>
-          <div>5</div>
+          <div>6</div>
         </div>
         <h2>Meetup</h2>
-        <div className={style.date}>May 28, 7:00 PM</div>
+        <div className={style.date}>June 24, 7:00 PM</div>
         <div className={style.location}>
           <img src="/static/location.svg" alt="location pin" />
           <p>Znajomi Znajomych - Warsaw</p>
@@ -23,20 +23,37 @@ const Meetup = () => (
         <h1>Agenda</h1>
         <hr />
         <div className={style.talks}>
-          <h2>First topic</h2>
-          <h3>Speaker:</h3>
-          <h3>Language:</h3>
+          <h2>Building observable Elixir services [EN]</h2>
+          <h3>Speaker: Paweł Szafran</h3>
+          <h3>Language: English</h3>
           <p>
-            Topic's description Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Nam congue placerat turpis ac vestibulum. Duis at
-            dui sit amet odio porta porta. Nam vitae vestibulum eros, fringilla
-            sodales ipsum. Nulla a tortor ac lorem tincidunt aliquet ac vel
-            urna.
+            When you run Elixir services at scale, how can you tell how well
+            they are performing? Let's explore practical tips to increase the
+            observability of Elixir services, with some live examples.
           </p>
           <div className={style.level}>
             <h3>Level:</h3>
             <img src="/static/phial_full.svg" alt="full phial" />
             <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_empty.svg" alt="empty phial" />
+            <img src="/static/phial_empty.svg" alt="empty phial" />
+          </div>
+          <h2>Beware of dragons! The charms of remote work [PL]</h2>
+          <h3>Speaker: Jan Ziniewicz & Łukasz Łażewski</h3>
+          <h3>Language: Polish</h3>
+          <p>
+            Jan and Łukasz will discuss working remotely from an employee and
+            employers perspective. What are the common pitfalls and how to avoid
+            them. What rules do they recommend to introduce to make this kind of
+            workflow work.
+          </p>
+          <div className={style.level}>
+            <h3>Level:</h3>
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_empty.svg" alt="empty phial" />
             <img src="/static/phial_empty.svg" alt="empty phial" />
           </div>
         </div>
