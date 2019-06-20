20d843e870e1c582addcc85db7a3b6301f714773
anitakniaziew <anita.kniaziew@gmail.com>
2019-06-20 13:48:20 +0200

    add mailto in footer contact, fix main page arrow

--- components/NextMeetup/index.js
+++ components/NextMeetup/index.js
@@ -3,48 +3,49 @@ import Link from "next/link";
 
 const Meetup = () => (
   <div className={style.next}>
-    <img
-      src="./static/arrow_down.svg"
-      alt="arrow down"
-      className={style.arrow}
-    />
-    <div className={style.details}>
-      <div className={style.num}>
-        <div>#</div>
-        <div>5</div>
-      </div>
-      <h2>Meetup</h2>
-      <div className={style.date}>May 28, 7:00 PM</div>
-      <div className={style.location}>
-        <img src="/static/location.svg" alt="location pin" />
-        <p>Znajomi Znajomych - Warsaw</p>
-      </div>
+    <div className={style.arrow}>
+      <img src="./static/arrow_down.svg" alt="arrow down" />
     </div>
-    <div className={style.agenda}>
-      <h1>Agenda</h1>
-      <hr />
-      <div className={style.talks}>
-        <h2>First topic</h2>
-        <h3>Speaker:</h3>
-        <h3>Language:</h3>
-        <p>
-          Topic's description Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nam congue placerat turpis ac vestibulum. Duis at dui sit amet
-          odio porta porta. Nam vitae vestibulum eros, fringilla sodales ipsum.
-          Nulla a tortor ac lorem tincidunt aliquet ac vel urna.
-        </p>
-        <div className={style.level}>
-          <h3>Level:</h3>
-          <img src="/static/phial_full.svg" alt="full phial" />
-          <img src="/static/phial_full.svg" alt="full phial" />
-          <img src="/static/phial_empty.svg" alt="empty phial" />
+    <div className={style.container}>
+      <div className={style.details}>
+        <div className={style.num}>
+          <div>#</div>
+          <div>5</div>
+        </div>
+        <h2>Meetup</h2>
+        <div className={style.date}>May 28, 7:00 PM</div>
+        <div className={style.location}>
+          <img src="/static/location.svg" alt="location pin" />
+          <p>Znajomi Znajomych - Warsaw</p>
         </div>
       </div>
-      <button className={style.signButton}>
-        <Link href="/speak-up">
-          <a>Sign me up</a>
-        </Link>
-      </button>
+      <div className={style.agenda}>
+        <h1>Agenda</h1>
+        <hr />
+        <div className={style.talks}>
+          <h2>First topic</h2>
+          <h3>Speaker:</h3>
+          <h3>Language:</h3>
+          <p>
+            Topic's description Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Nam congue placerat turpis ac vestibulum. Duis at
+            dui sit amet odio porta porta. Nam vitae vestibulum eros, fringilla
+            sodales ipsum. Nulla a tortor ac lorem tincidunt aliquet ac vel
+            urna.
+          </p>
+          <div className={style.level}>
+            <h3>Level:</h3>
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_full.svg" alt="full phial" />
+            <img src="/static/phial_empty.svg" alt="empty phial" />
+          </div>
+        </div>
+        <button className={style.signButton}>
+          <Link href="/speak-up">
+            <a>Sign me up</a>
+          </Link>
+        </button>
+      </div>
     </div>
   </div>
 );
