d48ba1d5fb42b0a9924baca1def219c8947f1759
Kamil Kowalski <kowalski.tychy@gmail.com>
2019-06-24 00:02:13 +0200

    Add Google Analytics tracking

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,4 +1,6 @@
+import React from "react";
 import Head from "next/head";
+import ReactGA from "react-ga";
 import "./style.scss";
 
 const LayoutWrapper = props => props.children;
@@ -11,27 +13,51 @@ const createTitle = title => {
   }
 }
 
-const Layout = props => (
-  <LayoutWrapper>
-    <Head>
-      <title>{createTitle(props.title)}</title>
-      <meta
-        name="description"
-        content="warsaw.ex is a community dedicated to Elixir, Erlang, OTP
-        and functional programming, organizing monthly meetups in Warsaw." />
-      <link
-        href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,900"
-        rel="stylesheet"
-      />
-      <link
-        rel="shortcut icon"
-        type="image/png"
-        href="/static/favicon.png"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    {props.children}
-  </LayoutWrapper>
-);
+export const initGA = () => {
+  console.log('GA init')
+  ReactGA.initialize('UA-142587988-1')
+}
+
+export const logPageView = () => {
+  console.log(`Logging pageview for ${window.location.pathname}`)
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+
+class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+
+    logPageView();
+  }
+
+  render() {
+    return (
+      <LayoutWrapper>
+        <Head>
+          <title>{createTitle(this.props.title)}</title>
+          <meta
+            name="description"
+            content="warsaw.ex is a community dedicated to Elixir, Erlang, OTP
+            and functional programming, organizing monthly meetups in Warsaw." />
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,900"
+            rel="stylesheet"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            href="/static/favicon.png"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        {this.props.children}
+      </LayoutWrapper>
+    );
+  }
+}
 
 export default Layout;
