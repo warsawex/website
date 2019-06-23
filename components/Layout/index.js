5ef3fbe603f3343630ed503092ef4651362c69e1
Kamil Kowalski <kowalski.tychy@gmail.com>
2019-06-24 00:20:06 +0200

    Remove console logs

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -14,12 +14,10 @@ const createTitle = title => {
 }
 
 export const initGA = () => {
-  console.log('GA init')
   ReactGA.initialize('UA-142587988-1')
 }
 
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`)
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
