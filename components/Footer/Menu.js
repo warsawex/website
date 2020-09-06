ead92c04b834ae5052aa0200d38db951d8c56626
Anita Kowalska <anita.kniaziew@gmail.com>
2020-09-06 20:22:43 +0200

    Add button and link hover

--- components/Footer/Menu.js
+++ components/Footer/Menu.js
@@ -1,19 +1,23 @@
-import style from "./style.scss";
-import Link from "next/link";
+import React from 'react';
 
-const Menu = () => (
-  <div className={style.menu}>
-    <h2>
-      <Link href="/speak-up">
-        <a>Speak Up</a>
-      </Link>
-    </h2>
-    <h2>
-      <Link href="/about-us">
-        <a>About Us</a>
-      </Link>
-    </h2>
-  </div>
-);
+import style from './style.scss';
+import Link from 'next/link';
+
+const Menu = () => {
+  return (
+    <div className={style.menu}>
+      <h2>
+        <Link href="/speak-up">
+          <a>Speak Up</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/about-us">
+          <a>About Us</a>
+        </Link>
+      </h2>
+    </div>
+  );
+};
 
 export default Menu;
